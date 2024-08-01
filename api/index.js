e1cae35fc3bc44cd0fd140be3bf09d9dcbdfcbe2
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-01 15:50:55 -0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const PORT = 3000 || process.env
+
+mongoose.connect(process.env.MONGO_DB_URL).then(()=>{
+    console.log('MongoDB Connected')
+}).catch((error)=> {
+    console.log(error)
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
