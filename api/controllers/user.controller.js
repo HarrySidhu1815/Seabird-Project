3ab86dcf355aaf4026ae7d43088e95685c6701b2
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-09 08:21:03 -0700

    delete user account route added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,21 @@
+import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
+
 export const test = (req, res)=> {
     res.json({
         message: 'API Routes are working'
     })
+}
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id){
+        return next(errorHandler(403, 'You can only delete your own account'))
+    }
+
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json({message: 'User has been deleted!'})
+    } catch (error) {
+        next(error)
+    }
 }
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { test } from '../controllers/user.controller.js'
+import { deleteUser, test } from '../controllers/user.controller.js'
+import { verifyUser } from '../utils/verifyUser.js'
 
 const router = express.Router()
 
 router.get('/', test)
+router.delete('/delete:id', verifyUser, deleteUser)
 
 export default router
