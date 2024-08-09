5d480753a5a0aa6fb64be97e027b880ba3ad4810
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-09 09:15:01 -0700

    curriculum lessons made expandable with dummy data

--- client/src/components/Resource/Resource.jsx
+++ client/src/components/Resource/Resource.jsx
@@ -1,62 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import arrow from "../../assets/arrow.png";
 import Lesson from "./Lesson";
-import classes from './Resource.module.css'
+import classes from "./Resource.module.css";
+import { getAllLessonBySubject, getAllSubjects } from "../../util/lesson";
 
 export default function Resource() {
+  const subjects = getAllSubjects();
+
   return (
     <div className={classes.resource}>
-      <div>
-        <h1>English</h1>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-      </div>
-      <div>
-        <h1>Mathematics</h1>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-      </div>
-      <div>
-        <h1>Science</h1>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-      </div>
-      <div>
-        <h1>Social Studies</h1>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-        <Lesson title="Lesson Title" level="Grade Level">
-          This is a description of the subject, information, learning outcomes,
-          lesson plan, and the kinds of classroom activities it may or may not
-          include.
-        </Lesson>
-      </div>
+      {subjects.map((subject) => {
+        const lessons = getAllLessonBySubject(subject);
+        const [isExpanded, setIsExpanded] = useState(false);
+
+        function handleExpandClick() {
+          setIsExpanded((prevState) => !prevState);
+        }
+        return (
+          <div key={subject}>
+            <div className={classes["lesson-header"]}>
+              <h1>{subject}</h1>
+              <button onClick={handleExpandClick}>
+                <img className={isExpanded ? classes.close : classes.open} src={arrow} alt="arrow" />
+              </button>
+            </div>
+            {isExpanded &&
+              lessons.map((lesson) => {
+                return (
+                  <Lesson
+                    key={lesson.id}
+                    title={lesson.title}
+                    level={lesson.level}
+                  >
+                    {lesson.description}
+                  </Lesson>
+                );
+              })}
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ client/src/util/lesson.js
@@ -0,0 +1,76 @@
+export const DUMMY_LESSONS = [
+    {
+        id: '1',
+        subject: 'English',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+    {
+        id: '2',
+        subject: 'English',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+    {
+        id: '3',
+        subject: 'Mathematics',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+    {
+        id: '4',
+        subject: 'Mathematics',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+    {
+        id: '5',
+        subject: 'Science',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+    {
+        id: '6',
+        subject: 'Science',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+    {
+        id: '7',
+        subject: 'Social Studies',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+    {
+        id: '8',
+        subject: 'Social Studies',
+        title: 'Lesson Title',
+        level: 'Grade Level',
+        description: 'This is a description of the subject, information, learning outcomes, lesson plan, and the kinds of classroom activities it may or may not include.',
+        packageLink: 'http://dummy_url.com'
+    },
+]
+
+export const getAllSubjects = () => {
+    const sujects = DUMMY_LESSONS.map(lesson => lesson.subject)
+    return [...new Set(sujects)]
+}
+
+export const getAllLessonBySubject = (subject) => {
+    const lessons = DUMMY_LESSONS.filter(lesson => lesson.subject === subject)
+    return lessons
+}
