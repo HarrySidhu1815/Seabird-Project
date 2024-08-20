4382a669dadaa5302675f27db61822cd821b109d
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-20 13:39:26 -0700

    videos url added

--- client/src/components/BrowseVideo/VideoCard.jsx
+++ client/src/components/BrowseVideo/VideoCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import classes from './VideoCard.module.css'
 import { useInView } from 'react-intersection-observer';
 import ReactPlayer from 'react-player';
@@ -9,28 +9,53 @@ export default function VideoCard({video}) {
   //   threshold: 0.5,  
   // });
 
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [thumbnail, setThumbnail] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    const canvasElement = canvasRef.current;
+    const context = canvasElement.getContext('2d');
+
+    videoElement.onloadeddata = () => {
+      videoElement.currentTime = 2;
+    };
+
+    videoElement.onseeked = () => {
+      context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+      const thumbnailURL = canvasElement.toDataURL('image/png');
+      setThumbnail(thumbnailURL);
+    };
+  }, []);
+ 
   return (
     <div className={classes['card-container']}>
       {!isPlaying ? (
+        thumbnail ? (
         <img
-          src={`https://img.youtube.com/vi/${video.id}/hqdefault.jpg`}
+          src={thumbnail}
           alt={video.title}
           onClick={() => setIsPlaying(true)}
           className={classes.img}
-        />
+        />) : 
+        (<p>Loading ThumbNail</p>)
       ) : (
         <ReactPlayer
           className={classes.img}
-          url={video.url}
+          url={video.videoUrl}
           controls={true}
+          height='60%'
+          width='100%' 
         />
       )}
       <div className={classes['card-decription']}>
         <h2>{video.title}</h2>
         <p><i>{video.topic}</i><br/>Features: {video.speakers.join(', ')}</p>
       </div>
+      <video ref={videoRef} src={video.videoUrl} style={{ display: 'none' }} />
+      <canvas ref={canvasRef} style={{ display: 'none' }} />
     </div>
   )
 }
