f5b224a73ef22980b604a16122dda2c1e57e6437
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-08-29 12:51:56 -0700

    names added

--- client/src/components/CurriculumHeader/CurriculumHeader.jsx
+++ client/src/components/CurriculumHeader/CurriculumHeader.jsx
@@ -11,12 +11,19 @@ export default function CurriculumHeader() {
         Carlson. The students were asked to create new curriculum that would
         highlight the knowledge and wisdom of Seabird Island Elders. To
         accomplish this, the students drew directly from the Elders’ voices that
-        appear in the 99 video mini-documentaries. <br/><br/>In addition, Dr. Dawn Marsden
-        (Mississaugas of Scugog Island FN) and Seabird Island member Chelsea
-        Forseth have also developed curriculum resources that are available
-        here. <br/><br/>More resources will continue to be added as they are developed.<br/><br/>
+        appear in the 99 video mini-documentaries. <br />
+        <br />
+        In addition, Dr. Dawn Marsden (Mississaugas of Scugog Island FN) and
+        Seabird Island member Chelsea Forseth have also developed curriculum
+        resources that are available here. <br />
+        <br />
+        More resources will continue to be added as they are developed.
+        <br />
+        <br />
         The UFV Collaboratorium students who contributed to the curriculum
-        resources showcased here include XXXXXX
+        resources showcased here include Tara-Lynn Kozma-Perrin, Alexis Klassen,
+        Noa Brooks, Alex de Boer, Bethany Zimmerman, Chloe Belanger, Aliyah
+        Friesen, Zachary Mattie, Gurpreet Kaur Saini
       </p>
     </div>
   );
